0615ce89907204048b92dc6133b8a9fd641c656c
Harsh Shandilya <me@msfjarvis.dev>
2020-05-30 03:45:22 +0530

    src: simplify redirection logic
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- src/handler.ts
+++ src/handler.ts
@@ -4,17 +4,21 @@ const GITHUB_USERNAME = 'msfjarvis'
 export async function handleRequest(request: Request): Promise<Response> {
   const relURL = request.url.replace(BASE_URL, '')
   const URLparts = relURL.split('/')
-  if (URLparts[0] == 'gcp') {
-    return Response.redirect(
-      `https://github.com/${GITHUB_USERNAME}/${URLparts[1]}/commit/${URLparts[2]}`,
-      301,
-    )
-  } else if (URLparts[0] == 'g') {
-    return Response.redirect(
-      `https://github.com/${GITHUB_USERNAME}/${URLparts[1]}`,
-      301,
-    )
-  } else {
-    return new Response(`Illegal redirect path: ${relURL}`)
+  if (URLparts[0] === 'g') {
+    switch (URLparts.length) {
+      case 2:
+        return Response.redirect(
+          `https://github.com/${GITHUB_USERNAME}/${URLparts[1]}`,
+          301,
+        )
+      case 3:
+        return Response.redirect(
+          `https://github.com/${GITHUB_USERNAME}/${URLparts[1]}/commit/${URLparts[2]}`,
+          301,
+        )
+      default:
+        break
+    }
   }
+  return new Response(`Illegal redirect path: ${relURL}`)
 }
