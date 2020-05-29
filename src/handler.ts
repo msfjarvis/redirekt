843610731e355fbb16a3159d9215c2739a0f9272
Harsh Shandilya <me@msfjarvis.dev>
2020-05-30 00:06:10 +0530

    Initial commit
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

---
+++ src/handler.ts
@@ -0,0 +1,20 @@
+export const BASE_URL = 'https://msfjarvis.dev/'
+export const GITHUB_USERNAME = 'msfjarvis'
+
+export async function handleRequest(request: Request): Promise<Response> {
+  const relURL = request.url.replace(BASE_URL, '')
+  const URLparts: Array<string> = relURL.split('/')
+  if (URLparts[0] == 'gcp') {
+    return Response.redirect(
+      `https://github.com/${GITHUB_USERNAME}/${URLparts[1]}/commit/${URLparts[2]}`,
+      301,
+    )
+  } else if (URLparts[0] == 'g') {
+    return Response.redirect(
+      `https://github.com/${GITHUB_USERNAME}/${URLparts[1]}`,
+      301,
+    )
+  } else {
+    return new Response(`Illegal redirect path: ${relURL}`)
+  }
+}
