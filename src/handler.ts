a0a51e003f232be02162ce537e44cd8ef9015ebf
Harsh Shandilya <me@msfjarvis.dev>
2020-06-04 13:29:46 +0530

    src: cleanup and simplify
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,7 @@
 const BASE_URL = 'https://msfjarvis.dev/'
 const DOWNLOAD_URL = 'https://download.msfjarvis.dev/'
 const GITHUB_USERNAME = 'msfjarvis'
+const GITHUB_URL = `https://github.com/${GITHUB_USERNAME}`
 
 export async function handleRequest(request: Request): Promise<Response> {
   if (request.url.includes(DOWNLOAD_URL)) {
@@ -9,23 +10,20 @@ export async function handleRequest(request: Request): Promise<Response> {
   }
   const relURL = request.url.replace(BASE_URL, '')
   const URLparts = relURL.split('/')
-  if (URLparts[0] === 'g') {
-    switch (URLparts.length) {
-      case 1:
-        return Response.redirect(`https://github.com/${GITHUB_USERNAME}`, 301)
-      case 2:
-        return Response.redirect(
-          `https://github.com/${GITHUB_USERNAME}/${URLparts[1]}`,
-          301,
-        )
-      case 3:
-        return Response.redirect(
-          `https://github.com/${GITHUB_USERNAME}/${URLparts[1]}/commit/${URLparts[2]}`,
-          301,
-        )
-      default:
-        break
-    }
+  switch (URLparts[0]) {
+    case 'g':
+      switch (URLparts.length) {
+        case 1:
+          return Response.redirect(`${GITHUB_URL}`, 301)
+        case 2:
+          return Response.redirect(`${GITHUB_URL}/${URLparts[1]}`, 301)
+        case 3:
+          return Response.redirect(
+            `${GITHUB_URL}/${URLparts[1]}/commit/${URLparts[2]}`,
+            301,
+          )
+      }
+    default:
+      return fetch(request)
   }
-  return fetch(request)
 }
