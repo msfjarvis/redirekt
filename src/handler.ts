14a1c7408808940edc25cafde38d9c40b2879845
Harsh Shandilya <me@msfjarvis.dev>
2020-06-21 04:04:47 +0530

    src: add ga shortcut for APS
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- src/handler.ts
+++ src/handler.ts
@@ -2,13 +2,15 @@ const BASE_URL = 'https://msfjarvis.dev/'
 const DOWNLOAD_URL = 'https://download.msfjarvis.dev/'
 const DOWNLOAD_DEST_URL = 'https://dl.msfjarvis.dev/'
 const GITHUB_USERNAME = 'msfjarvis'
+const APS_SLUG = 'Android-Password-Store/Android-Password-Store'
 const GITHUB_URL = `https://github.com/${GITHUB_USERNAME}`
+const APS_GITHUB_URL = `https://github.com/${APS_SLUG}`
 
 export async function handleRequest(request: Request): Promise<Response> {
   if (request.url.startsWith(DOWNLOAD_URL)) {
     return redirectDownload(request, DOWNLOAD_URL, DOWNLOAD_DEST_URL)
   } else if (request.url.startsWith(BASE_URL)) {
-    return redirectGithub(request, BASE_URL, GITHUB_URL)
+    return redirectGithub(request, BASE_URL, GITHUB_URL, APS_GITHUB_URL)
   } else {
     return fetch(request)
   }
@@ -29,13 +31,14 @@ async function redirectGithub(
   request: Request,
   baseDomain: string,
   githubUrl: string,
+  apsUrl: string,
 ): Promise<Response> {
   const urlParts = request.url.replace(baseDomain, '').split('/')
   switch (urlParts[0]) {
     case 'g':
       switch (urlParts.length) {
         case 1:
-          return Response.redirect(`${githubUrl}`, 301)
+          return Response.redirect(githubUrl, 301)
         case 2:
           return Response.redirect(`${githubUrl}/${urlParts[1]}`, 301)
         case 3:
@@ -43,6 +46,15 @@ async function redirectGithub(
             `${githubUrl}/${urlParts[1]}/commit/${urlParts[2]}`,
             301,
           )
+      }
+    case 'ga':
+      switch (urlParts.length) {
+        case 1:
+          return Response.redirect(apsUrl, 301)
+        case 2:
+          return Response.redirect(`${apsUrl}/commit/${urlParts[1]}`, 301)
+        case 3:
+          return Response.redirect(`${apsUrl}/issues/${urlParts[2]}`, 301)
       }
     default:
       return fetch(request)
