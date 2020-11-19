7bff7b6dc9f9eb84e6fb8636c5dc64fc2ab2afe2
Harsh Shandilya <me@msfjarvis.dev>
2020-11-19 14:49:57 +0530

    src: remove BTC stuff
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- src/handler.ts
+++ src/handler.ts
@@ -3,39 +3,15 @@ const GITHUB_USERNAME = 'msfjarvis'
 const APS_SLUG = 'Android-Password-Store/Android-Password-Store'
 const GITHUB_URL = `https://github.com/${GITHUB_USERNAME}`
 const APS_GITHUB_URL = `https://github.com/${APS_SLUG}`
-const PAYID_DATA = {
-  harsh: {
-    addresses: [
-      {
-        paymentNetwork: 'BTC',
-        environment: 'MAINNET',
-        addressDetailsType: 'CryptoAddressDetails',
-        addressDetails: {
-          address: '1LbHbfTq1ZtE8u3qipCTJTcJYptvzc54aB',
-        },
-      },
-    ],
-    payId: 'harsh$msfjarvis.dev',
-  },
-}
 
 export async function handleRequest(request: Request): Promise<Response> {
-  if (request.headers.get('Accept') == 'application/btc-mainnet+json') {
-    return sendPayID(request)
-  } else if (request.url.startsWith(BASE_URL)) {
+  if (request.url.startsWith(BASE_URL)) {
     return redirectGitHub(request)
   } else {
     return fetch(request)
   }
 }
 
-async function sendPayID(request: Request): Promise<Response> {
-  switch (request.url.split('/').reverse()[0]) {
-    case 'harsh':
-      return new Response(JSON.stringify(PAYID_DATA.harsh))
-  }
-  return fetch(request)
-}
 
 async function redirectGitHub(request: Request): Promise<Response> {
   const urlParts = request.url.replace(BASE_URL, '').split('/')
