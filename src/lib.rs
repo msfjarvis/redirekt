ccddd31617135884b076970c25b3d42e06aca42c
Harsh Shandilya <me@msfjarvis.dev>
2022-04-10 23:34:41 +0530

    Show a basic stats page

--- src/lib.rs
+++ src/lib.rs
@@ -24,16 +24,25 @@ async fn list_keys(kv: &KvStore, prefix: String) -> Vec<Key> {
 
 #[event(fetch)]
 pub async fn main(_req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
-    if let Ok(kv) = env.kv("BINDING") {
-        for key in list_keys(&kv, "stats_".to_string()).await {
-            console_log!("{}", key.name);
-        }
-    } else {
-        console_log!("Failed to get KV");
-    }
-
     // Optionally, get more helpful error messages written to the console in the
     // case of a panic.
     utils::set_panic_hook();
-    Response::error("", 400)
+
+    return if let Ok(kv) = env.kv("BINDING") {
+        let mut result = String::new();
+        result.push_str("<html><head><title>Stats page</title></head><body><table><tr><th>URL</th><th>Count</th></tr>");
+        for key in list_keys(&kv, "stats_".to_string()).await {
+            if let Some(value) = kv.get(&key.name).cache_ttl(600).text().await.unwrap_or_else(|_| None) {
+                result.push_str(&format!(
+                    "<tr><td>{key}</td><td>{value}</td></tr>",
+                    key = key.name.strip_prefix("stats_").unwrap()
+                ));
+            }
+        }
+        result.push_str("</body>");
+        Response::from_html(result)
+    } else {
+        console_log!("Failed to get KV");
+        Response::error("", 400)
+    };
 }
